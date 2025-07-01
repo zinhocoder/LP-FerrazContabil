a0ce80472e5750af1ad8fab22e1f089d52e3d416
zinhocoder <roberto.goncalves2003@hotmail.com>
2025-07-01 11:35:34 -0300

    Adding gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next'
+import Head from 'next/head'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Ferraz Contabilidade | Soluções Contábeis Inteligentes para o Sucesso do Seu Negócio',
-  description: 'A Ferraz Contabilidade oferece serviços contábeis completos para empresas e profissionais autônomos. Especialistas em abertura de empresas, gestão fiscal, folha de pagamento, consultoria tributária e muito mais. Atendimento personalizado e foco em resultados.',
+  description:
+    'A Ferraz Contabilidade oferece serviços contábeis completos para empresas e profissionais autônomos. Especialistas em abertura de empresas, gestão fiscal, folha de pagamento, consultoria tributária e muito mais. Atendimento personalizado e foco em resultados.',
 }
 
 export default function RootLayout({
@@ -13,7 +15,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P3M57Q88');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P3M57Q88"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   )
 }
